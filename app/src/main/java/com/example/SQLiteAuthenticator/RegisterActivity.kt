f9e7476cdad09150a7d749802a6166fc5dbed29c
Batın Gökçe Savran <batinsavran00@gmail.com>
2024-04-16 16:55:23 +0300

    Checking the data required for registration

--- app/src/main/java/com/example/SQLiteAuthenticator/RegisterActivity.kt
+++ app/src/main/java/com/example/SQLiteAuthenticator/RegisterActivity.kt
@@ -14,6 +14,30 @@ class RegisterActivity : AppCompatActivity() {
         setContentView(R.layout.activity_register)
     }
 
+    fun register(view: View){
+        val nameView=findViewById<EditText>(R.id.editTextText)
+        val name=findViewById<EditText>(R.id.editTextText).text.toString()
+        val surname=findViewById<EditText>(R.id.editTextText2).text.toString()
+        val email=findViewById<EditText>(R.id.editTextTextEmailAddress2).text.toString()
+        val password=findViewById<EditText>(R.id.editTextTextPassword2).text.toString()
+        val password_repeat=findViewById<EditText>(R.id.editTextTextPassword3).text.toString()
+
+        if (name.equals("") || surname.equals("") || email.equals("") || password.equals("") || password_repeat.equals("") ){
+            Toast.makeText(this@RegisterActivity, "Tüm Bilgileri Eksiksiz Giriniz.", Toast.LENGTH_SHORT).show()
+        }else{
+            if (password.equals(password_repeat)){
+                val passwordLength = password.length
+                if (passwordLength >= 8){
+
+                }else{
+                    Toast.makeText(this@RegisterActivity, "Şifreniz en az 8 karakter olmalı.", Toast.LENGTH_SHORT).show()
+                }
+            }else{
+                Toast.makeText(this@RegisterActivity, "Girdiğiniz şifreler birbirini tutmuyor.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
     fun openLogin(view: View){
         startActivity(Intent(this, LoginActivity::class.java))
     }
