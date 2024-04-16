0e3e716cb2673d05720f975a57c66a865a043e09
Batın Gökçe Savran <batinsavran00@gmail.com>
2024-04-16 16:52:16 +0300

    Completed SqlLite Database Connection

--- app/src/main/java/com/example/SQLiteAuthenticator/MainActivity.kt
+++ app/src/main/java/com/example/SQLiteAuthenticator/MainActivity.kt
@@ -9,8 +9,28 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.R
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var context: Context
+    lateinit var db: SQLiteDatabase
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        context = this
+        val dbFile = context.getDatabasePath("testDB")
+        Log.i("DBtest", dbFile.exists().toString())
+        db = openOrCreateDatabase("testDB", Context.MODE_PRIVATE, null)
+
+        db.execSQL("CREATE TABLE users (userid INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, surname TEXT, email TEXT, password TEXT)")
+
+        val userId = intent.getIntExtra("userId", -1)
+        if (userId > 0) {
+
+        } else {
+            val loginIntent = Intent(this, LoginActivity::class.java)
+            startActivity(loginIntent)
+            finish()
+        }
     }
 }
