3e15101ec903d6cc173db700be7d9c0683822960
Batın Gökçe Savran <batinsavran00@gmail.com>
2024-05-05 16:55:07 +0300

    View Binding Added and Clean Code Applied to the Lost Password Activity Page

--- app/src/main/java/com/example/SQLiteAuthenticator/LostPasswordActivity.kt
+++ app/src/main/java/com/example/SQLiteAuthenticator/LostPasswordActivity.kt
@@ -3,10 +3,15 @@ package com.example.SQLiteAuthenticator
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.R
+import com.example.myapplication.databinding.ActivityLostPasswordBinding
+import com.example.myapplication.databinding.ActivityRegisterBinding
 
 class LostPasswordActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLostPasswordBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_lost_password)
+        binding = ActivityLostPasswordBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
