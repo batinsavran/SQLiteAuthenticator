0ef5264c530f2de551e23f2cb9061d552362845d
Batın Gökçe Savran <batinsavran00@gmail.com>
2024-05-05 16:54:44 +0300

    View Binding Added and Clean Code Applied to the Register Activity Page

--- app/src/main/java/com/example/SQLiteAuthenticator/RegisterActivity.kt
+++ app/src/main/java/com/example/SQLiteAuthenticator/RegisterActivity.kt
@@ -5,55 +5,70 @@ import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
 import android.view.View
-import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.myapplication.R
+import com.example.myapplication.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
 
-    lateinit var db: SQLiteDatabase
+    private lateinit var binding: ActivityRegisterBinding
+    private lateinit var db: SQLiteDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         db = this.openOrCreateDatabase("TestDB", AppCompatActivity.MODE_PRIVATE, null)
     }
 
-    fun register(view: View){
-        val nameView=findViewById<EditText>(R.id.editTextText)
-        val name=findViewById<EditText>(R.id.editTextText).text.toString()
-        val surname=findViewById<EditText>(R.id.editTextText2).text.toString()
-        val email=findViewById<EditText>(R.id.editTextTextEmailAddress2).text.toString()
-        val password=findViewById<EditText>(R.id.editTextTextPassword2).text.toString()
-        val password_repeat=findViewById<EditText>(R.id.editTextTextPassword3).text.toString()
+    fun register(view: View) {
+        val name = binding.editTextText.text.toString()
+        val surname = binding.editTextText2.text.toString()
+        val email = binding.editTextTextEmailAddress2.text.toString()
+        val password = binding.editTextTextPassword2.text.toString()
+        val passwordRepeat = binding.editTextTextPassword3.text.toString()
 
-        if (name.equals("") || surname.equals("") || email.equals("") || password.equals("") || password_repeat.equals("") ){
-            Toast.makeText(this@RegisterActivity, "Tüm Bilgileri Eksiksiz Giriniz.", Toast.LENGTH_SHORT).show()
-        }else{
+        if (name.isEmpty() || surname.isEmpty() || email.isEmpty() || password.isEmpty() || passwordRepeat.isEmpty()) {
+            Toast.makeText(
+                this@RegisterActivity,
+                "Tüm Bilgileri Eksiksiz Giriniz.",
+                Toast.LENGTH_SHORT
+            ).show()
+        } else {
             val result: Cursor = db.rawQuery("SELECT * FROM users WHERE email = ?", arrayOf(email))
-            if (result.count > 0){
-                Toast.makeText(this@RegisterActivity, "Bu mail adresi zaten kayıtlı.", Toast.LENGTH_SHORT).show()
+            if (result.count > 0) {
+                Toast.makeText(
+                    this@RegisterActivity,
+                    "Bu mail adresi zaten kayıtlı.",
+                    Toast.LENGTH_SHORT
+                ).show()
             } else {
-                if (password.equals(password_repeat)){
-                    val passwordLength = password.length
-                    if (passwordLength >= 3){
-                        //Regex ile kontrol edebiliriz.
-                        db.execSQL("insert into users (name, surname, email, password) values ('$name', '$surname', '$email', '$password')")
-                        Toast.makeText(this@RegisterActivity, "Kayıt Başarılı.", Toast.LENGTH_SHORT).show()
+                if (password == passwordRepeat) {
+                    if (password.length >= 3) {
+                        db.execSQL("INSERT INTO users (name, surname, email, password) VALUES ('$name', '$surname', '$email', '$password')")
+                        Toast.makeText(this@RegisterActivity, "Kayıt Başarılı.", Toast.LENGTH_SHORT)
+                            .show()
                         startActivity(Intent(this, LoginActivity::class.java))
                     } else {
-                        Toast.makeText(this@RegisterActivity, "Şifreniz en az 3 karakter olmalı.", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(
+                            this@RegisterActivity,
+                            "Şifreniz en az 3 karakter olmalı.",
+                            Toast.LENGTH_SHORT
+                        ).show()
                     }
                 } else {
-                    Toast.makeText(this@RegisterActivity, "Girdiğiniz şifreler birbirini tutmuyor.", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(
+                        this@RegisterActivity,
+                        "Girdiğiniz şifreler birbirini tutmuyor.",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
             }
         }
     }
 
-    fun openLogin(view: View){
+    fun openLogin(view: View) {
         startActivity(Intent(this, LoginActivity::class.java))
     }
 }
