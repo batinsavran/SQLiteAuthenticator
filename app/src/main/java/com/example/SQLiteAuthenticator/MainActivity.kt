a3c2e602670d0ef0feda1f246d95fa2c38bda98f
Batın Gökçe Savran <batinsavran00@gmail.com>
2024-05-05 16:54:31 +0300

    View Binding Added and Clean Code Applied to the Main Activity Page

--- app/src/main/java/com/example/SQLiteAuthenticator/MainActivity.kt
+++ app/src/main/java/com/example/SQLiteAuthenticator/MainActivity.kt
@@ -4,31 +4,24 @@ import android.content.Context
 import android.content.Intent
 import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.example.myapplication.R
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var context: Context
-    lateinit var db: SQLiteDatabase
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var db: SQLiteDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        context = this
-        val dbFile = context.getDatabasePath("TestDB")
-        Log.i("DBtest", dbFile.exists().toString())
         db = openOrCreateDatabase("TestDB", Context.MODE_PRIVATE, null)
-
-        db.execSQL("CREATE TABLE users (userid INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, surname TEXT, email TEXT, password TEXT)")
-
+        db.execSQL("CREATE TABLE IF NOT EXISTS users (userid INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, surname TEXT, email TEXT, password TEXT)")
 
         val userId = intent.getIntExtra("userId", -1)
-        if (userId > 0) {
-
-        } else {
+        if (userId <= 0) {
             val loginIntent = Intent(this, LoginActivity::class.java)
             startActivity(loginIntent)
             finish()
